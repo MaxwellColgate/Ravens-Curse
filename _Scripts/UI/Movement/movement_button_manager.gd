d33953238ab77b9d4707ed5bf86795f73739e594
MrColgate <bankruptman321@gmail.com>
2025-07-03 19:25:44 +0930

    Fixed weird shoe animation, move arrows still look kinda funky

--- _Scripts/UI/Movement/movement_button_manager.gd
+++ _Scripts/UI/Movement/movement_button_manager.gd
@@ -3,6 +3,10 @@ class_name MoveButton
 
 ## Manges the logic related to the player's movement button
 
+
+# The movement button's AnimationPlayer
+@export var movement_button_anim: AnimationPlayer
+
 # The movement SFX player
 @export var movement_sfx: AudioStreamPlayer
 
@@ -24,3 +28,7 @@ func _ready():
 func _on_movement_button_pressed() -> void:
 	is_moving = !is_moving
 	MovementManager.change_movement_state(is_moving)
+
+
+func _on_button_up() -> void:
+	movement_button_anim.play("release_shoe")
